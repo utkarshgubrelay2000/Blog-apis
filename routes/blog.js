be51a6607265bb956bb1e82f2fcbbdefd7236bf7
utkarshgubrelay <utkarshgubrelay2000@gmail.com>
2021-01-17 15:47:21 +0530

    routes get ad

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var blogController=require('../controller/blogController');
 const verifyAdmin = require('../middleware/verifyAdmin');
 /* GET home page. */
+router.get('/postBlog',(req,res)=>{
+  res.render('author')
+})
+router.get('/api/admin/secret',verifyAdmin,blogController.adminpanel)
 router.post('/postBlog',verifyAdmin,blogController.postBlog,err=>{
   console.log('error while signup user')
 })
